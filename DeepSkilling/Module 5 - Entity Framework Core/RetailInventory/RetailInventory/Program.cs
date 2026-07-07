67805122315b65a562011f0e80c19663ba231ca8
Zeba0207 <shaikzebafathima6@gmail.com>
2026-07-07 17:58:26 +0530

    Completed Module 5 - Entity Framework Core

---
+++ DeepSkilling/Module 5 - Entity Framework Core/RetailInventory/RetailInventory/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
+using RetailInventory.DTOs;
+
+namespace RetailInventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new AppDbContext();
+
+            var products = context.Products
+                .Include(p => p.Category)
+                .Select(p => new ProductDTO
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    CategoryName = p.Category.Name
+                })
+                .ToList();
+
+            Console.WriteLine("Product DTOs:");
+            Console.WriteLine();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Name} - ₹{product.Price} - {product.CategoryName}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
