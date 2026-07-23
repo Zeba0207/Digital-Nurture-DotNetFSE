5f47298e3f5097a0d162e202c4995548259fa6dc
Zeba0207 <shaikzebafathima6@gmail.com>
2026-07-23 15:26:39 +0530

    Add files via upload

---
+++ UpSkilling/Module 3/Module3_CSharp_ADONET/Exercise02_ValueVsReference/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise02_ValueVsReference
+{
+    class Person
+    {
+        public string Name;
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+    internal class Program
+    {
+        static void ModifyValue(int number)
+        {
+            number = 100;
+        }
+
+        static void ModifyReference(Person person)
+        {
+            person.Name = "Bob";
+        }
+
+        static void Main(string[] args)
+        {
+            int value = 10;
+            Person person = new Person("Alice");
+
+            Console.WriteLine("Before Method Call:");
+            Console.WriteLine("Value Type = " + value);
+            Console.WriteLine("Reference Type = " + person.Name);
+
+            ModifyValue(value);
+            ModifyReference(person);
+
+            Console.WriteLine("\nAfter Method Call:");
+            Console.WriteLine("Value Type = " + value);
+            Console.WriteLine("Reference Type = " + person.Name);
+        }
+    }
+}
---
+++ UpSkilling/Module 3/Module3_CSharp_ADONET/Exercise03_PrimaryConstructors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise03_PrimaryConstructors
+{
+    public class Person(string firstName, string lastName, int age)
+    {
+        public string FirstName { get; } = firstName;
+        public string LastName { get; } = lastName;
+        public int Age { get; } = age;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name : {FirstName} {LastName}");
+            Console.WriteLine($"Age  : {Age}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Zeba", "Fathima", 20);
+            person.DisplayInfo();
+        }
+    }
+}
