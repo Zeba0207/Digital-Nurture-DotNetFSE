ca4ad915a63d916b3ca769b22643497dbfe0e079
Zeba0207 <shaikzebafathima6@gmail.com>
2026-07-24 17:27:08 +0530

    Add files via upload

---
+++ UpSkilling/Module 3/Module3_CSharp_ADONET/Module3_CSharp_ADONET/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise01_SetupEnvironment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("C# Development Environment Setup Successfully!");
+        }
+    }
+}
